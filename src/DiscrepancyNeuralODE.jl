373711531287a3b7bad335ba998e196f79923475
Ilya Orson Sandoval <ilya.orson@gmail.com>
2020-11-16 20:59:58 -0600

    prepare for more systems and add gradient example

--- src/DiscrepancyNeuralODE.jl
+++ src/DiscrepancyNeuralODE.jl
@@ -1,94 +1,55 @@
 # adapted from https://diffeqflux.sciml.ai/dev/examples/feedback_control/
 
-using DiffEqFlux, Flux, Optim, OrdinaryDiffEq
+module DiscrepancyNeuralODE
+
+using DiffEqFlux, Flux, Optim, OrdinaryDiffEq, Zygote
 using UnicodePlots: lineplot, lineplot!
-
-function system!(du, u, p, t, controller)
-    # fixed parameters
-    α, β, γ, δ = 0.5, 1.0, 1.0, 1.0
-
-    # neural network outputs controls taken by the system
-    y1, y2 = u
-    c1, c2 = controller(u, p)
-
-    # dynamics of the controlled system
-    y1_prime = -(c1 + α * c1^2) * y1 + δ * c2
-    y2_prime = (β * c1 - γ * c2) * y1
-
-    # update in place
-    du[1] = y1_prime
-    du[2] = y2_prime
-end
-
-# define objective function to optimize
-function loss(params, prob, tsteps)
-    sol = solve(prob, Tsit5(), p = params, saveat = tsteps)
-    return -Array(sol)[2, end]  # second variable, last value, maximize
-end
+using ClearStacktrace  # nicer stacktraces (unnecesary in julia 1.6)
 
 # handy terminal plots
-function unicode_plotter(u1, u2, c1, c2)
+function unicode_plotter(states, controls)
+
+    ylim = Iterators.flatten((states, controls)) |> extrema
     plt = lineplot(
-        c1,
+        states[1,:],
         title = "State and Control Evolution",
-        name = "c1",
+        name = "x1",
         xlabel = "step",
-        ylim = extrema([u1; u2; c1; c2]),
+        ylim = ylim,
     )
-    lineplot!(plt, c2, name = "c2", color = :blue)
-    lineplot!(plt, u1, name = "u1", color = :yellow)
-    lineplot!(plt, u2, name = "u2", color = :magenta)
+    for (i, s) in enumerate(eachrow(states[2:end,:]))
+        lineplot!(plt, collect(s), name = "x$(i+1)")
+    end
+    for (i, c) in enumerate(eachrow(controls))
+        lineplot!(plt, collect(c), name = "c$i")
+    end
     return plt
 end
 
 # simulate evolution at each iteration and plot it
 function plot_simulation(params, loss, prob, tsteps)
-    @info "Objective" -loss
-    sol = solve(prob, Tsit5(), p = params, saveat = tsteps)
-    u1s, u2s = Real[], Real[]
-    c1s, c2s = Real[], Real[]
-    for u in sol.u
-        u1, u2 = u
-        push!(u1s, u1)
-        push!(u2s, u2)
-        c1, c2 = controller(u, params)
-        push!(c1s, c1)
-        push!(c2s, c2)
+    @info "Objective" loss
+    solution = solve(prob, Tsit5(), p = params, saveat = tsteps)
+
+    # construct arrays with the same type used by the integrator
+    elements_type = eltype(solution.t)
+    states = Array(solution)
+
+    total_steps = size(states, 2)
+    state_dimension = size(states, 1)
+    control_dimension = length(controller(solution.u[1], params))
+
+    # regenerate controls from controller
+    controls = zeros(elements_type, control_dimension, total_steps)
+    for (step, state) in enumerate(solution.u)
+        controls[:, step] = controller(state, params)
     end
-    display(unicode_plotter(u1s, u2s, c1s, c2s))
-    return false
+    display(unicode_plotter(states, controls))
+    return false  # if return true, then optimization stops
 end
 
-# intial conditions and timepoints
-u0 = [1.0f0, 0.0f0]
-tspan = (0.0f0, 1.0f0)
-tsteps = 0.0f0:0.1:1.0f0
+function runner(script)
+    include(joinpath(@__DIR__, "$script.jl"))
+end
 
-# set arquitecture of neural network controller
-controller = FastChain(
-    FastDense(2, 20, tanh),
-    FastDense(20, 20, tanh),
-    FastDense(20, 2),
-    (x, p) -> 5 * σ.(x),  # bounds for controllers
-)
-
-# model weights are destructured into a vector of parameters
-θ = initial_params(controller)
-
-# set differential equation problem and solve it
-dudt!(du, u, p, t) = system!(du, u, p, t, controller)
-prob = ODEProblem(dudt!, u0, tspan, θ)
-# sol = solve(prob, Tsit5(), abstol = 1e-8, reltol = 1e-6)
-
-# closures to comply with required interface
-loss(params) = loss(params, prob, tsteps)
-plotting_callback(params, loss) = plot_simulation(params, loss, prob, tsteps)
-
-# Hic sunt dracones
-result = DiffEqFlux.sciml_train(
-    loss,
-    θ,
-    BFGS(initial_stepnorm = 0.01),
-    cb = plotting_callback,
-    allow_f_increases = false,
-)
+end # module
---
+++ src/batch_reactor.jl
@@ -0,0 +1,71 @@
+function system!(du, u, p, t, controller)
+    # fixed parameters
+    α, β, γ, δ = 0.5f0, 1.0f0, 1.0f0, 1.0f0
+
+    # neural network outputs controls taken by the system
+    y1, y2 = u
+    c1, c2 = controller(u, p)
+
+    # dynamics of the controlled system
+    y1_prime = -(c1 + α * c1^2) * y1 + δ * c2
+    y2_prime = (β * c1 - γ * c2) * y1
+
+    # update in-place
+    @inbounds begin
+        du[1] = y1_prime
+        du[2] = y2_prime
+    end
+end
+
+# define objective function to optimize
+function loss(params, prob, tsteps)
+    sol = solve(prob, Tsit5(), p = params, saveat = tsteps)  # integrate ODE system
+    return -Array(sol)[2, end]  # second variable, last value, maximize
+end
+
+# initial conditions and timepoints
+u0 = [1.0f0, 0.0f0]
+tspan = (0.0f0, 1.0f0)
+tsteps = 0.0f0:0.001f0:1.0f0
+
+# set arquitecture of neural network controller
+controller = FastChain(
+    FastDense(2, 20, tanh),
+    FastDense(20, 20, tanh),
+    FastDense(20, 2),
+(x, p) -> 5 * σ.(x),  # controllers ∈ (0, 5)
+)
+
+# model weights are destructured into a vector of parameters
+θ = initial_params(controller)
+
+# set differential equation problem and solve it
+dudt!(du, u, p, t) = system!(du, u, p, t, controller)
+prob = ODEProblem(dudt!, u0, tspan, θ)
+
+# closures to comply with required interface
+loss(params) = loss(params, prob, tsteps)
+plotting_callback(params, loss) = plot_simulation(params, loss, prob, tsteps)
+
+# Hic sunt dracones
+result = DiffEqFlux.sciml_train(
+    loss,
+    θ,
+    # BFGS(initial_stepnorm = 0.01),
+    LBFGS(),
+    # maxiters=10,
+    cb = plotting_callback,
+    allow_f_increases = true,
+)
+
+# https://fluxml.ai/Zygote.jl/latest/#Taking-Gradients
+# an example of how to extract gradients
+@show eltype(θ)
+@show l₀ = loss(θ)
+@time ∇θ = Zygote.gradient(loss, θ)[1]
+@show typeof(∇θ)
+@show eltype(∇θ)
+h = 1e-2  # eltype(θ)(1e-3)
+@show eltype(h * ∇θ)
+@show lₕ = loss(θ + h * ∇θ)
+@show ∇L = lₕ - l₀
